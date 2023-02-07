8b7a1906029f3df2c99121bc469f429977af317e
M. Mudassir Awan <121383917+Mudasssirr@users.noreply.github.com>
2023-02-07 14:25:23 +0500

    JavaScript A4 Ch12-13
    Added JavaScript Assignment 4 from Chapter 12 to 13

---
+++ JavaScript A4 Ch12-13/app.js
@@ -0,0 +1,83 @@
+//Chapter 12-13
+
+//Question 1
+
+// var num1 = prompt("Enter number or letter");
+// var letter1 = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+// var letter2 = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"; 
+// if(num1 === letter1){
+//     alert("Given letters are Uppercase");
+// }
+// if(num1 === letter2){
+//     alert("Given letters are lowercase");
+// }
+
+//Question 2
+// var num2 = prompt("Enter a number 1");
+// var num3 = prompt("Enter a number 2");
+// if(num2 > num3){
+//     alert("number 1 is greater than number 2")
+// }
+// if(num3 > num2){
+//     alert("number 2 is greater than 1")
+// }
+// if(num2 === num3){
+//     alert("number 1 and number 2 are equal")
+// }
+
+//Question 3
+// var num4 = prompt("Enter Number")
+// if(num4 === "1, 2, 3, 4, 5, 6, 7, 8, 9, 10"){
+//     alert("Given number is positive");
+// }
+// if(num4 === "-1,-2,-3,-4,-5,-6,-7,-8,-9,-10"){
+//     alert("Given number is negative");
+// }
+// if(num4 === "0"){
+//     alert("Given number is 0");
+// }
+
+//Questionn 4
+// var letter3 = prompt("Enter vowels");
+// if(letter3 === "a,e,i,o,u"){
+//     alert("Given letters are vowels");
+// }
+// if(letter3 !== "a,e,i,o,u"){
+//     alert("False");
+// }
+
+//Question 5
+// var pwd = 55531333;
+// var pwd2 = prompt("Enter Password");
+// if(pwd2 === pwd){
+//     alert("Correct! The password you entered matches the original password.")
+// }
+// if(pwd2 !== pwd){
+//     alert("Incorrect Password")
+// }
+
+//Question 6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good evening";
+// }
+// else{
+// greeting = "Good day";
+// }
+
+//Question 7
+// var greeting;
+// var time = 1900;
+// if(time >= 0000 && time < 1200){
+//     greeting = "Good Morning";
+// }
+// if(time >= 1200 && time < 1700){
+//     greeting = "Good Afternoon";
+// }
+// if(time >= 1700 && time < 2100){
+//     greeting = "Good Evening";
+// }
+// if(time >= 2100 && time <= 2359){
+//     greeting = "Good Night";
+// }
